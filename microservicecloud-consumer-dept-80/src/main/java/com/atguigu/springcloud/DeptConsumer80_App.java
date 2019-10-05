cf944095cb8787a360c8a5c0dff0c4c901353eac
cypBoy <38341360+cypBoy@users.noreply.github.com>
2019-10-05 23:56:40 +0800

    Initial commit

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import com.atguigu.myrule.MySelfRule;
+
+@SpringBootApplication
+@EnableEurekaClient
+//在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效
+//自定义配置类不能放在@ComponetScan所扫描的当前包下及其子包下
+//@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+public class DeptConsumer80_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptConsumer80_App.class, args);
+	}
+}
